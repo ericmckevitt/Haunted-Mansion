e5e8249332c115009f2ab95c0ffb2879e33f0239
Eric McKevitt <erictmckevitt@gmail.com>
2025-03-09 16:26:26 -0600

    initial commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,46 @@
+//using System.Numerics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+
+    private PlayerMotor motor;
+    private PlayerLook look;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+
+        onFoot.Jump.performed += ctx => motor.Jump();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Tell motor to move based on the value from the movement action
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcesssLook(onFoot.Look.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
