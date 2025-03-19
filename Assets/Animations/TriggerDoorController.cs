eccea238ee525a88c26c0babf51005298cda09da
Eric McKevitt <erictmckevitt@gmail.com>
2025-03-19 16:15:48 -0600

    added basic level and door open/close animation

---
+++ Assets/Animations/TriggerDoorController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class TriggerDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor = null;
+    [SerializeField] private Transform player;
+    [SerializeField] private float detectionRange = 3.0f;
+
+    private bool isDoorOpen = false;
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, player.position) <= detectionRange)
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                if (isDoorOpen)
+                {
+                    myDoor.Play("DoorClose", 0, 0.0f);
+                    isDoorOpen = false;
+                }
+                else
+                {
+                    myDoor.Play("door", 0, 0.0f);
+                    isDoorOpen = true;
+                }
+            }
+        }
+    }
+}
