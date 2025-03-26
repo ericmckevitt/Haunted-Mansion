dd02910bd35d4d91276d71fe936752e5dfee3f63
Eric McKevitt <erictmckevitt@gmail.com>
2025-03-25 19:13:59 -0600

    player can't move while fixing light

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,37 +1,48 @@
-//using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
+    public static InputManager Instance;
+
     private PlayerInput playerInput;
     private PlayerInput.OnFootActions onFoot;
 
     private PlayerMotor motor;
     private PlayerLook look;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool inputEnabled = true;
+
     void Awake()
     {
+        Instance = this;
+
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
 
         motor = GetComponent<PlayerMotor>();
         look = GetComponent<PlayerLook>();
 
-        onFoot.Jump.performed += ctx => motor.Jump();
+        onFoot.Jump.performed += ctx => {
+            if (inputEnabled)
+                motor.Jump();
+        };
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        // Tell motor to move based on the value from the movement action
-        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+        if (inputEnabled)
+        {
+            motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+        }
     }
 
     private void LateUpdate()
     {
-        look.ProcesssLook(onFoot.Look.ReadValue<Vector2>());
+        if (inputEnabled)
+        {
+            look.ProcesssLook(onFoot.Look.ReadValue<Vector2>());
+        }
     }
 
     private void OnEnable()
@@ -42,5 +53,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
     private void OnDisable()
     {
         onFoot.Disable();
+    }
+
+    public static void DisableInput()
+    {
+        Instance.inputEnabled = false;
+    }
+
+    public static void EnableInput()
+    {
+        Instance.inputEnabled = true;
     }
 }
--- Assets/Scripts/LightProximinity.cs
+++ Assets/Scripts/LightProximinity.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
 
 public class LightProximinity : MonoBehaviour
 {
-
     private bool playerNearby = false;
+    private bool isFixingLight = false;
+    public InputManager instance = null;
+
+    void Start()
+    {
+        instance = GetComponent<InputManager>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -17,16 +25,39 @@ public class LightProximinity : MonoBehaviour
             playerNearby = false;
     }
 
-
-
     void Update()
     {
-        if (playerNearby && Input.GetKeyDown(KeyCode.L)) {
-            Transform lightChild = transform.GetChild(0);
-            if (!lightChild.gameObject.activeSelf) {
-                lightChild.gameObject.SetActive(true);
-                Debug.Log("Light turned on!");
-            }
+        if (playerNearby && Input.GetKeyDown(KeyCode.L) && !isFixingLight)
+        {
+            StartCoroutine(FixLight());
         }
+    }
+
+    IEnumerator FixLight()
+    {
+        isFixingLight = true;
+        DisablePlayerInput(); // You can connect this to your input system later
+
+        yield return new WaitForSeconds(4f);
+
+        Transform lightChild = transform.GetChild(0);
+        if (!lightChild.gameObject.activeSelf)
+        {
+            lightChild.gameObject.SetActive(true);
+            Debug.Log("Light turned on after fixing!");
+        }
+
+        EnablePlayerInput(); // Re-enable controls after delay
+        isFixingLight = false;
+    }
+
+    void DisablePlayerInput()
+    {
+        InputManager.DisableInput();
+    }
+
+    void EnablePlayerInput()
+    {
+        InputManager.EnableInput();
     }
 }
