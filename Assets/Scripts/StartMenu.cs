0113f0a7a9a563ff5772d680a2289bf9dddf11a3
jdiamond61 <jackson@dbacksys.com>
2025-04-07 23:19:50 -0600

    Added Start splash screen with working start button and diffiuclty selector. we can update the panel to change the background. it seems to be doing a good job freezing the game too i think.

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI; // If you use Button/Dropdown from the built-in UI
+using TMPro;          // If you use TextMeshPro elements
+
+public class StartMenu : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject startMenuCanvas; 
+    // Drag your new 'StartMenuCanvas' from the Hierarchy to this in the Inspector
+
+    [SerializeField] private TMP_Dropdown difficultyDropdown;
+    // Or a regular Dropdown if you're not using TextMeshPro
+
+    private bool gameStarted = false;
+
+    private void Start()
+    {
+        // Freeze the game by setting timescale to 0
+        Time.timeScale = 0f;
+
+        // Make sure the canvas is active at the start
+        if (startMenuCanvas != null)
+            startMenuCanvas.SetActive(true);
+    }
+
+    // This will be called by the "Start Game" button
+    public void OnStartGamePressed()
+    {
+        // Unfreeze the game
+        Time.timeScale = 1f;
+
+        // Hide the Start Menu
+        if (startMenuCanvas != null)
+            startMenuCanvas.SetActive(false);
+
+        gameStarted = true;
+    }
+
+    // This will be called by your difficulty dropdown (if you have one)
+    public void OnDifficultyChanged(int value)
+    {
+        // 'value' corresponds to the index of the dropdown option
+        // Implement your own logic here for difficulty
+        switch(value)
+        {
+            case 0: // e.g. "Easy"
+                Debug.Log("Difficulty set to Easy");
+                break;
+            case 1: // e.g. "Medium"
+                Debug.Log("Difficulty set to Medium");
+                break;
+            case 2: // e.g. "Hard"
+                Debug.Log("Difficulty set to Hard");
+                break;
+        }
+    }
+}
